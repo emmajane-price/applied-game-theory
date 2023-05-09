c0b63df2c8274f9a67440e046907630c60cc3f90
Emma Price <ejprice54321@gmail.com>
2023-05-08 20:46:24 -0500

    commit remaining files

--- project/project/src/App.js
+++ project/project/src/App.js
@@ -1,45 +1,51 @@
-import React, { useState } from "react";
-import { Service as service } from "./Service.js";
 
+// Popover is based on: https://www.npmjs.com/package/material-ui-popup-state#popover-1
+// Icon usage is based on: https://fontawesome.com/v5/docs/web/use-with/react
+// Additional React usage help from: https://react.dev/learn
+// Deployment is done with GH pages: https://github.com/gitname/react-gh-pages
+
+import React, { useState } from "react";
+import { FaHome, FaTree, FaInfoCircle } from "react-icons/fa";
+import { Service as service } from "./Service.js";
+import Popover from '@mui/material/Popover';
+import {
+  usePopupState,
+  bindTrigger,
+  bindPopover,
+} from 'material-ui-popup-state/hooks'
 import './App.css';
 
-const SinglePayoff = ({userInput, onUserInput}) => {
-  return (
-    <input 
-      type="text" 
-      value={userInput} 
-      placeholder="Search..."
-      onChange={(e) => onUserInput(e.target.value)}/>
-  )
-}
 
-const Outcome = ({userInputs}) => {
-    return (<p>{service.determineOutputText(userInputs)}</p>);
-}
-
-const NormalFrom = ({userInputs}) => {
+const NormalFrom = ({userInputs, selected}) => {
   return (
-    <div className="card">
+    <div>
+      <h4>Game Normal Form &nbsp;
+        <BasicPopover popoverContent={"Each cell in this table represents a possible game outcome and each number within the cell is the payoff " +
+        "for that option for that community. For example if the Abate, Pollute is 20, -30 then Community 1 has a payoff " +
+        "of 20 for that option and Community 2 has a payoff of -30. The darker cell is the Nash Equilibrium, or the option " +
+        "where neither community can improve their expected payoff by picking a different strategy, assuming the other " +
+        "community does not change their strategy."}/>
+      </h4>
       <table>
         <tbody>
           <tr>
               <th colSpan="2" rowSpan="2"></th>
-              <th colSpan="2">Community 2</th>
+              <th colSpan="2">Community 1</th>
           </tr>
           <tr>
               <th>Abate</th>
               <th>Pollute</th>
           </tr>
           <tr>
-              <th rowSpan="2">Community 1</th>
+              <th rowSpan="2">Community 2</th>
               <th>Abate</th>
-              <td>{userInputs.user1[0]}, {userInputs.user2[0]}</td>
-              <td>{userInputs.user1[1]}, {userInputs.user2[1]}</td>
+              <td className={(selected === 0) ? 'dark-blue' : null}>{userInputs.user1[0]}, {userInputs.user2[0]}</td>
+              <td className={(selected === 1) ? 'dark-blue' : null}>{userInputs.user1[1]}, {userInputs.user2[1]}</td>
           </tr>
           <tr>
               <th>Pollute</th>
-              <td>{userInputs.user1[2]}, {userInputs.user2[2]}</td>
-              <td>{userInputs.user1[3]}, {userInputs.user2[3]}</td>
+              <td className={(selected === 2) ? 'dark-blue' : null}>{userInputs.user1[2]}, {userInputs.user2[2]}</td>
+              <td className={(selected === 3) ? 'dark-blue' : null}>{userInputs.user1[3]}, {userInputs.user2[3]}</td>
           </tr>
         </tbody>
       </table>
@@ -47,93 +53,247 @@ const NormalFrom = ({userInputs}) => {
   )
 }
 
-const ResourcePool = ({resources}) => {
-  return (<div><span>Remaining resource pool at end of game: {resources}</span></div>)
+const Houses = ({communitySize}) => {
+  let community = [];
+  for (let i=0; i<communitySize; i++) {
+    community.push(
+      <i className='home'>
+        <FaHome key={i} size='3rem'/>
+      </i>
+    )
+  }
+  return (community)
 }
 
-const UserUtility = ({user1Utility, user2Utility, handleUtilityInputs}) => {
+const Trees = ({resourceList}) => {
+  const treeList = [];
+  for (let i=0; i<resourceList.length; i++) {
+    const pixelSize = ((0.5+resourceList[i].size)*16).toString() + 'px';
+    if (resourceList[i].alive) {
+      treeList.push(
+        <FaTree className={'tree-item alive'} size={pixelSize}/>
+      )
+    } else {
+      treeList.push(
+        <FaTree className={'tree-item dead'} size={pixelSize}/>
+      )
+    }
+  }
+  return ( treeList )
+}
+
+const ResourcePool = ({resourceList}) => {
   return (
-    <div className="card">
-      <form className="utilityForm">
-        Where does community one's value lie? 
-        <div className="slidecontainer">
-          <input type="range" value={user1Utility} onInput={(e) => handleUtilityInputs(0, parseInt(e.target.value))} min="60" max="140" className="slider"/>
-          <label className="env">Environment</label>
-          <label className="growth">Growth</label>
-          <p>{user1Utility}</p>
+    <div>
+      <div className="card">
+        <h4>Resources &nbsp;
+          <BasicPopover popoverContent={"The trees below represent the environmental resources available " +
+          "to the two communities. After you jump forward 100 years the trees will change, either dying if " +
+          "the environment is overused, or growing if underused."}/>
+        </h4>
+        <div className="resourcePool">
+          <Trees resourceList={resourceList}/>
         </div>
-        Where does community two's value lie?
-        <div className="slidecontainer">
-          <input type="range" value={user2Utility} onInput={(e) => handleUtilityInputs(1, parseInt(e.target.value))} min="60" max="140" className="slider"/>
-          <label className="env">Environment</label>
-          <label className="growth">Growth</label>
-          <p>{user2Utility}</p>
-        </div>
-      </form>
+      </div>
+    </div>
+  )
+}
+const BasicPopover = ({popoverContent}) => {
+  const popupState = usePopupState({
+    variant: 'popover',
+    popupId: 'demoPopover',
+  })
+  return (
+    <span>
+      <FaInfoCircle variant="contained" {...bindTrigger(popupState)}/>
+      <Popover
+        {...bindPopover(popupState)}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+      <p className="popover" sx={{ p: 2 }}>{popoverContent}</p>
+      </Popover>
+    </span>
+  )
+}
+
+const Button = ({label, className, onClick}) => {
+  return <button className={'preset ' +className} onClick={() => onClick()}>{label}</button>
+}
+
+const Presets = ({startingInputs, selectedPreset, handlePresets}) => {
+  const buttons = [];
+  for (let i = 0; i < startingInputs.length; i++) {
+    buttons.push(<Button key={i} 
+      label={startingInputs[i].name}
+      className={(selectedPreset === i)?'dark-blue':'light-blue'}
+      onClick={() => handlePresets(i)}/>)
+  }
+  return (
+    <div>
+      <h4>Presets &nbsp;
+          <BasicPopover popoverContent={"Each of the following presets represents a common " + 
+          "game structure that can be meaningfully applied to climate change."}/>
+        </h4>
+      {buttons}
     </div>
   );
 }
 
-const Main = ({startingValue}) => {
+const UserUtility = ({utility, user, communitySize, handleUtilityInputs}) => {
+  return (
+    <form className="utilityForm">
+      <div className="slidecontainer utilityContainer1">
+        <Houses communitySize={communitySize}/>
+        <h6>Where does community {user}'s value lie?</h6>
+        <input type="range" value={utility} onInput={(e) => handleUtilityInputs(user - 1, parseInt(e.target.value))} min="60" max="140" className="slider"/>
+        <span className="env">Environment</span>
+        <span className="growth">Growth</span>
+      </div>
+    </form>
+  )
+}
+const UtilitySection = ({user1Utility, user2Utility, handleUtilityInputs, communitySize}) => {
+  return (
+    <div>
+      <h4>Community Utility &nbsp;
+          <BasicPopover popoverContent={"Rather than use the presets above, you can also directly decide how  " + 
+          "environmental or growth focused the communities are."}/>
+        </h4>
+      <UserUtility
+        utility={user1Utility}
+        user={1}
+        communitySize={communitySize[0]}
+        handleUtilityInputs={handleUtilityInputs}
+      />
+      <UserUtility
+        utility={user2Utility}
+        user={2}
+        communitySize={communitySize[1]}
+        handleUtilityInputs={handleUtilityInputs}
+      />
+    </div>
+  );
+}
+
+const Main = ({startingInputs, startingResources}) => {
   const [userInputs, setUserInputs] = useState({user1: [0,0,0,0], user2: [0,0,0,0]});
-  const [resources, setResources] = useState(100);
+  const [resourceList, setResourceList] = useState(startingResources);
+  const [selected, setSelected] = useState(-1);
+  const [selectedPreset, setSelectedPreset] = useState(-1);
+  const [communitySize, setCommunitySize] = useState([20,20]);
   const [user1Utility, setUser1Utility] = useState(100);
   const [user2Utility, setUser2Utility] = useState(100);
 
-  const handleChange = (userName, item, value) => {
-    userInputs[userName][item] = parseInt(value);
-    setUserInputs(userInputs);
-  }
 
   const handleUtilityInputs = (user, utility) => {
+    if (resourceList !== startingResources){
+      setResourceList(startingResources);
+      setCommunitySize([20,20]);
+    }
     if (user) {
       setUser2Utility(utility);
-      userInputs.user2 = [100-(utility), (utility)-100, 100-(utility), (utility)-100];
+      userInputs.user2 = service.createUser2Form(utility);
     } else {
       setUser1Utility(utility);
-      userInputs.user1 = [100-(utility), 100-(utility), (utility)-100, (utility)-100]
+      userInputs.user1 = service.createUser1Form(utility);
     };
     setUserInputs(userInputs);
-  }
-  const submitUtilities = (event) => {
-    event.preventDefault();
-    setResources(service.calculateEndResources(user1Utility, user2Utility));
+    setSelected(service.determineOutputText(userInputs));
   }
 
+  const handlePresets = (i) => {
+    if (resourceList !== startingResources){
+      setResourceList(startingResources);
+      setCommunitySize([20,20]);
+    }
+    let [user1, user2] = service.calculateUtilityFromNash(startingInputs[i])
+    setUser1Utility(user1);
+    setUser2Utility(user2);
+    setUserInputs(startingInputs[i].normalForm);
+    setSelectedPreset(i);
+    setSelected(startingInputs[i].nash);
+  }
+
+  const submitUtilities = (event) => {
+    event.preventDefault();
+    setResourceList(service.calculateEndResources(user1Utility, user2Utility, resourceList));
+    setCommunitySize(service.calculateCommunitySize(user1Utility, user2Utility));
+  }
+
+  const resetUtilities = (event) => {
+    event.preventDefault();
+    setUserInputs({user1: [0,0,0,0], user2: [0,0,0,0]});
+    setSelectedPreset(-1);
+    setResourceList(startingResources);
+    setCommunitySize([20,20]);
+  }
   return (
-    <div className="page">
+    <div className="container">
+      <div className="header">
+        <h1>Climate Change Game Theory Simulator</h1>        
+        <p> Click on each <FaInfoCircle/> to learn more about how each component works. 
+            Once you're ready to try it out, click the Jump 100 Years button!</p>
+      </div>
       <div className="card">
-        <UserUtility 
+        <form className="submit" onSubmit={(e) => submitUtilities(e)}>
+          <button type="submit"><h6>Jump 100 Years</h6></button>
+        </form>
+      </div>
+      <div className="card">
+        <form className="submit" onSubmit={(e) => resetUtilities(e)}>
+          <button type="submit"><h6>Reset Game</h6></button>
+        </form>
+      </div>
+      <div className="card normalForm">
+        <NormalFrom
+          userInputs={userInputs}
+          selected={selected}
+        />
+      </div>
+      <div className="card presets">
+        <Presets 
+          startingInputs={startingInputs}
+          selectedPreset={selectedPreset}
+          handlePresets={handlePresets}
+        />
+      </div>
+      <div className="card utilityContainer">
+        <UtilitySection 
           user1Utility={user1Utility}
           user2Utility={user2Utility}
           handleUtilityInputs={handleUtilityInputs}
+          communitySize={communitySize}
         />
       </div>
-      <NormalFrom
-        userInputs={userInputs}
-      />
-      <Outcome 
-        userInputs={userInputs}
-      />
-      <ResourcePool 
-        resources={resources}
-      />
-      <form onSubmit={(e) => submitUtilities(e)}>
-        <button type="submit">Submit</button>
-      </form>
+      <div className="results">
+        <ResourcePool
+          resourceList={resourceList}
+        />
+      </div>
     </div>
   )
 };
 
+const STARTING_INPUT = [
+  {name: "Prisoners", normalForm: {user1: [15,-30,30,-15], user2: [15,30,-30,-15]}, nash: 3},
+  {name: "Commons", normalForm: {user1: [5,-10,10,-5], user2: [5,10,-10,-5]}, nash: 3},
+  {name: "Chicken", normalForm: {user1: [15,-15,30,-30], user2: [15,30,-15,-30]}, nash: 1},
+  {name: "Harmony", normalForm: {user1: [30,15,-15,-30], user2: [30,-15,15,-30]}, nash: 0},
+  {name: "Stag Hunt", normalForm: {user1: [30,-30,15,-15], user2: [30,15,-30,-15]}, nash: 2},
+];
 
-const STARTING_INPUT = {
-  prisoners: {user1: [10,-40,40,-10], user2: [10,40,-40,-10]},
-  commons: {user1: [3,1,4,2], user2: [3,4,1,2]},
-  chicken: {user1: [3,1,4,2], user2: [3,4,1,2]},
-  harmony: {user1: [3,1,4,2], user2: [3,4,1,2]},
-  stagHunt: {user1: [3,1,4,2], user2: [3,4,1,2]},
-};
+const RESOURCE_LIST = service.generateResourceList();
 
 export default function App() {
-  return <Main startingValue={STARTING_INPUT} />;
+  return <Main 
+    startingInputs={STARTING_INPUT}
+    startingResources={RESOURCE_LIST}
+  />;
 }
--- project/project/src/Service.js
+++ project/project/src/Service.js
@@ -7,38 +7,75 @@ export const Service = {
         const max1b = Math.max(user1[1],user1[3]);
         const max2a = Math.max(user2[0],user2[1]);
         const max2b = Math.max(user2[2],user2[3]);
-        let text = "";
+        let selected;
         
-        if (user1[0] === max1a && user1[1] === max1b) {
-          text = text + " \n User 1 dominant strategy is abate";
-        } else if (user1[2] === max1a && user1[3] === max1b) {
-          text = text + " \n User 1 dominant strategy is pollute";
-        } else {
-          text = text + " \n User 1 has not dominant strategy";
-        }
-        if (user2[0] === max2a && user2[2] === max2b) {
-          text = text + " \n User 2 dominant strategy is abate";
-        } else if (user2[1] === max2a && user2[3] === max2b) {
-          text = text + " \n User 2 dominant strategy is pollute";
-        } else {
-          text = text + " \n User 2 has not dominant strategy";
-        }
         if (max1a === user1[0] && max1b === user1[1] && max2a === user2[0] && max2b === user2[2]) {
-          text = text + " \n Abate, Abate is the dominant strategy";
+          selected = 0;
         } else if (max1a === user1[2] && max1b === user1[3] && max2a === user2[1] && max2b === user2[3]) {
-          text = text + " \n Pollute, Pollute is the dominant strategy";
+          selected = 3;
         } else if (max1a === user1[0] && max1b === user1[1] && max2a === user2[1] && max2b === user2[3]) {
-          text = text + " \n Abate, Pollute is the dominant strategy";
+          selected = 1;
         } else if (max1a === user1[2] && max1b === user1[3] && max2a === user2[0] && max2b === user2[2]) {
-          text = text + " \n Pollute, Abate is the dominant strategy";
-        } else {
-          text = text + " \n There is no dominant strategy";
+          selected = 2;
         }
-        return text;
+        return selected;
       }
   },
-  calculateEndResources: (user1Utility, user2Utility) => {
-    const endingResources = 2600 - ((((12.5*(user1Utility/100)) + (12.5*(user2Utility/100)))) * 100)
-    return endingResources;
+  calculateEndResources: (user1Utility, user2Utility, startingResourceList) => {
+    const endingResources = parseInt(2600 - ((((12.5*(user1Utility/100)) + (12.5*(user2Utility/100)))) * 100)),
+      resourceList = JSON.parse(JSON.stringify(startingResourceList));
+      console.log(endingResources)
+    if (endingResources >= resourceList.length) {
+      for (let i=resourceList.length; i<endingResources; i++) {
+        resourceList.push({
+          alive: 1,
+          size: Math.random() * 2
+        });
+      }
+    } else if (endingResources > 0) {
+      for (let i=endingResources; i<resourceList.length; i++) {
+        resourceList[i].alive = 0;
+      }
+    } else {
+      for (let i=0; i<resourceList.length; i++) {
+        resourceList[i].alive = 0;
+      }
+    }
+    return resourceList;
+  },
+  calculateCommunitySize: (user1Utility, user2Utility) => {
+    return [parseInt(20*user1Utility/100), parseInt(20*user2Utility/100)];
+  },
+  createUser2Form: (user2Utility) => {
+    return [100-(user2Utility), (user2Utility)-100, 100-(user2Utility), (user2Utility)-100];
+  },
+  createUser1Form: (user1Utility) => {
+    return [100-(user1Utility), 100-(user1Utility), (user1Utility)-100, (user1Utility)-100];
+  },
+  calculateUtilityFromNash: (presetGame) => {
+    const absoluteVals = [Math.abs(presetGame.normalForm.user1[presetGame.nash]), Math.abs(presetGame.normalForm.user2[presetGame.nash])];
+    switch (presetGame.nash) {
+      case 0:
+        return [100-absoluteVals[0], 100-absoluteVals[1]];
+      case 1:
+        return [100-absoluteVals[0], 100+absoluteVals[1]];
+      case 2:
+        return [100+absoluteVals[0], 100-absoluteVals[1]];
+      case 3:
+        return [100+absoluteVals[0], 100+absoluteVals[1]];
+      default:
+        return "how did you get here??"
+
+    }
+  },
+  generateResourceList: () => {
+    const resourceList = [];
+    for(let i=0; i<100; i++) {
+      resourceList.push({
+        alive: 1,
+        size: Math.random() * 2
+      })
+    }
+    return resourceList;
   }
 }
